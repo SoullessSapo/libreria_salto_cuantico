def2930a076a4e92a4dc59b1d40743d47b764f62
Esteban Valencia Caicedo <93455764+SoullessSapo@users.noreply.github.com>
2022-10-04 13:00:09 -0500

    Update cuantico.py

--- cuantico.py
+++ cuantico.py
@@ -17,6 +17,32 @@ def prob_1(vector, vector_1):
     adj = np.matrix.getH(q)
     t = np.dot(r, adj)
     return t
+def prob_varianza(m,vector):
+    m = np.array(m)
+    r = np.linalg.det(m)
+    e = np.size(m)
+    c = [[i for i in range(e[0])] for j in range(e[1])]
+    for i in range(e[0]):
+        for j in range(e[1]):
+            c[i][j] = (-1) * (i + j) * r
+    for i in range(len(m)):
+            for j in range(len(m[0])):
+                if m[i][j] != c[i][j]:
+                    return False
+    vector = np.array(vector)
+    i = np.identity(e[0])
+    w = m *vector
+    w = np.transpose(w)
+    y = np.dot(w,vector)
+    h = m - i*y
+    g = h*h
+    varianza = np.dot(w,g)
+    varianza = np.dot(varianza,vector)
+    return varianza
+def prob_media(m):
+    m = np.array(m)
+    q = np.linalg.eigvals(m)
+    r = np.linalg.eig(m)
 def main():
     x = int(input("1,2"))
     if x ==1:
